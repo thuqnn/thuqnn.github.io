11d3592fb1c8c32e71fd7fb9404d01270098d359
thu phamqnn <thupham92qnn@gmail.com>
2019-06-17 14:15:50 +0700

    form validate

--- Form/Register_Validate_form/js/main.js
+++ Form/Register_Validate_form/js/main.js
@@ -2,55 +2,55 @@ $(document).ready(function() {
     $("#register-form").on("submit", function() {
         var idValid = true;
         if ($("#name").val().trim() == '') {
-            $("#name").next('small').text('Enter Name !');
+            $("#name").attr("placeholder", "Please Enter Your Name !");
             idValid = false;
         } else {
-            $("#name").next('small').text('');
+            $("#name").attr("placeholder", "");;
         }
         if ($("#pass").val().match(/^.{4,8}$/) == null) {
-            $("#pass").next('small').text('Enter Pass !');
+            $("#pass").attr("placeholder", "Please Enter Your Password !");
             idValid = false;
         } else {
-            $("#pass").next('small').text('');
+            $("#pass").attr("placeholder", "");;
         }
         if ($("#email").val().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) == null) {
-            $("#email").next('small').text('Enter Email !');
+            $("#email").attr("placeholder", "Please Enter Your Email !");
             idValid = false;
         } else {
-            $("#email").next('small').text('');
+            $("#email").attr("placeholder", "");;
         }
         if ($("#address").val().trim() == '') {
-            $("#address").next('small').text('Enter Address !');
+            $("#address").attr("placeholder", "Please Enter Your Address !");
             idValid = false;
         } else {
-            $("#address").next('small').text('');
+            $("#address").attr("placeholder", "");;
         }
         if ($("#phone").val().match(/^([\(]{1}[0-9]{3}[\)]{1}[\.| |\-]{0,1}|^[0-9]{3}[\.|\-| ]?)?[0-9]{3}(\.|\-| )?[0-9]{4}$/) == null) {
-            $("#phone").next('small').text('Enter Number Phone !');
+            $("#phone").attr("placeholder", "Please Enter Your Phone Number !");
             idValid = false;
         } else {
-            $("#phone").next('small').text('');
+            $("#phone").attr("placeholder", "");;
         }
         if ($("#linkFB").val().match(/(?<http>(http:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*)/) == null) {
-            $("#linkFB").next('small').text('Enter Link FaceBook !');
+            $("#linkFB").attr("placeholder", "Please Enter Your Link Facebook !");
             idValid = false;
         } else {
-            $("#linkFB").next('small').text('');
+            $("#linkFB").attr("placeholder", "");;
         }
 
         let gender = $('.gender').val();
         if ($(".gender:checked").length > 1 || $(".gender:checked").length == 0) {
-            $('#error').html('<br/><span id="error"> Please choose a gender !</span>');
+            $('.genderinfo').css("border-color", "red");
             idValid = false;
         } else {
-            $('#error').html('');
+            $('.genderinfo').css("border-color", "black");
         }
 
         if ($("#date").val().trim() == '') {
-            $("#date").next('small').text('Select Date of Birth !');
+            $("#date").css("border-color", "red");
             idValid = false;
         } else {
-            $("#date").next('small').text('');
+            $("#date").css("border-color", "black");
         }
         return idValid;
     });
@@ -58,7 +58,9 @@ $(document).ready(function() {
 
 function myFunction() {
     let uri = document.URL;
+    let urinew = document.URL.split('?')[0];
     let uri_dec = decodeURIComponent(uri);
-    let content = uri_dec.replace(/[&?]/g, '<br/>').replace(/[+=]/g, ' ');
-    $("#username").html(content);
+    let content = uri_dec.replace(/[&?]/g, '<br/>').replace(/[+]/g, ' ').replace(/[=]/g, ': ').replace('signup: Sign Up', '');
+    let total = content.replace(urinew, '').replace(/date/g, 'Date').replace(/gender/g, 'Gender').replace(/address/g, 'Address').replace(/phone/g, 'Phone').replace(/facebook/g, 'Facebook');
+    $("#username").html(total);
 }
