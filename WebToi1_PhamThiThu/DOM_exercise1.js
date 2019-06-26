45884cd82a222a80af62d0593c80be3725c1d7fe
thuqnn <phamthithuqn92@gmail.com>
2019-06-27 02:45:47 +0700

    jquerydom3

---
+++ WebToi1_PhamThiThu/DOM_exercise1.js
@@ -0,0 +1,12 @@
+function changeLogo() {
+    let lgYahoo = document.getElementById("hplogo");
+    lgYahoo.src = "https://securitynoob.in/images/honour/yahoo.png";
+    lgYahoo.srcset = "https://securitynoob.in/images/honour/yahoo.png";
+    return lgYahoo;
+}
+
+function changeText() {
+    let btnYahoo = document.getElementsByClassName("gNO89b");
+    btnYahoo[1].value = "Yahoo!";
+    return btnYahoo;
+}
