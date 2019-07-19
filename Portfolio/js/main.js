29d83a7095cd90a995ff4c01875a1dc726d20225
thuqnn <phamthithuqn92@gmail.com>
2019-07-19 14:32:37 +0700

    appmobile

---
+++ Portfolio/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    // Typed JS 
+    let typed = new Typed('#typed', {
+        strings: ["Web Developer", "Frontend Developer"],
+        typeSpeed: 30,
+        loop: true,
+        startDelay: 1e3,
+        backDelay: 3e3
+    });
+
+});
